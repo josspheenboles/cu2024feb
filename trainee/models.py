b779ff79315525481ed6c9d4f1c9c19092d56d43
jboles <eng.jospheenboles@gmail.com>
2024-01-31 23:36:50 +0200

    Day2-database migration Update trainee model

--- trainee/models.py
+++ trainee/models.py
@@ -5,6 +5,6 @@ class Trainee(models.Model):
     name=models.CharField(max_length=10)
     age=models.IntegerField(default=21)
     email=models.EmailField()
-    img=models.CharField(max_length=100)
+    img=models.CharField(max_length=100,null=True)
     createdat=models.DateTimeField(auto_now_add=True)
     updatedat=models.DateTimeField(auto_now=True)
