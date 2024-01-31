5635ae66eea73af2f05d5d0b4e3c18b287cc7965
jboles <eng.jospheenboles@gmail.com>
2024-01-31 23:53:35 +0200

    Day2-database list & get by id

--- trainee/urls.py
+++ trainee/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import  views
 urlpatterns=[
     path('List',views.traineelist),
+    path('<int:id>',views.traineedetails,name="trainee.details"),
 ]
--- trainee/views.py
+++ trainee/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def traineelist(request):
-    return  render(request,'trainee/index.html')
+    trainees=Trainee.objects.all()
+    context={'trainess':trainees}
+    return  render(request,'trainee/index.html',context)
+def traineedetails(request,id):
+    trainee = Trainee.objects.get(id=id)
+    context = {'trainee': trainee}
+    return render(request, 'trainee/traineedetails.html', context)
