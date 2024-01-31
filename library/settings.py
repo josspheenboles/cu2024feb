d1bbb61bf9b1a00d29c0051e22099a4552b4ce5c
jboles <eng.jospheenboles@gmail.com>
2024-01-31 21:47:43 +0200

    Day2-database configure database based in setting.py

--- library/settings.py
+++ library/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'iti',
+        'USER':'iti',
+        'PASSWORD':'123',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
