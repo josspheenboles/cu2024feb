087301b31b90f623c8af3748fcbc6ac659444a89
jboles <eng.jospheenboles@gmail.com>
2024-01-31 23:36:02 +0200

    Day2-database migration trainee model

--- trainee/models.py
+++ trainee/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Trainee(models.Model):
+    name=models.CharField(max_length=10)
+    age=models.IntegerField(default=21)
+    email=models.EmailField()
+    img=models.CharField(max_length=100)
+    createdat=models.DateTimeField(auto_now_add=True)
+    updatedat=models.DateTimeField(auto_now=True)
