0e433475566c6aeeb7e2cd795ddcc2845f7db739
jboles <eng.jospheenboles@gmail.com>
2024-02-04 00:21:22 +0200

    Day3- formsclean_data

--- trainee/forms.py
+++ trainee/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from .models import *
+from django.core.exceptions import ValidationError
 class TraineeForm(forms.Form):
     name = forms.CharField(required=True)
     age = forms.IntegerField(required=True)
     email = forms.EmailField()
     img = forms.ImageField()
+    def clean_name(self):
+        obj=Trainee.objects.get(name=self.cleaned_data['name']).exsists()
+        if obj:
+            raise ValidationError("Name Must Be unique")
