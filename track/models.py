d9cfd12b3ba291d0f8d0d77f56ff958236daffb3
jboles <eng.jospheenboles@gmail.com>
2024-02-04 00:57:28 +0200

    Day3 models relations fk in form

--- track/models.py
+++ track/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Track(models.Model):
     name=models.CharField(max_length=10)
+    @classmethod
+    def get_all_tracks(cls):
+        return [(obj.id,obj.name) for obj in cls.objects.all()]
+    def __str__(self):
+        return self.name
--- trainee/forms.py
+++ trainee/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from .models import *
+from track.models import *
 from django.core.exceptions import ValidationError
 class TraineeForm(forms.Form):
     name = forms.CharField(required=True)
     age = forms.IntegerField(required=True)
     email = forms.EmailField()
     img = forms.ImageField()
+    track=forms.ChoiceField(label='Track:',choices=Track.get_all_tracks())
     def clean_name(self):
         print(self.cleaned_data)
         obj=Trainee.objects.filter(name=self.cleaned_data['name']).exsists()
