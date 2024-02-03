7ce0ff26145ecb639e0070e5da19d53074da0dad
jboles <eng.jospheenboles@gmail.com>
2024-02-03 20:05:06 +0200

    Day3-image in model

--- trainee/models.py
+++ trainee/models.py
@@ -5,7 +5,7 @@ class Trainee(models.Model):
     name=models.CharField(max_length=10)
     age=models.IntegerField(default=21)
     email=models.EmailField()
-    img=models.CharField(max_length=100,null=True)
+    img=models.ImageField(upload_to='trainee/media',blank=True,null=True)
     createdat=models.DateTimeField(auto_now_add=True)
     updatedat=models.DateTimeField(auto_now=True)
 
