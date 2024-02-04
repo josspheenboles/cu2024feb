92b14e63405a8e444e6161499ebfc7c00ba05690
jboles <eng.jospheenboles@gmail.com>
2024-02-05 00:26:45 +0200

    Day4 auth app

--- track/urls.py
+++ track/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import  views
+from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('List',views.TrackList.as_view(),name='track.list'),
-    path('Create',views.TrackCreate.as_view(),name='track.create'),
+    path('Create',login_required(views.TrackCreate.as_view()),name='track.create'),
     path('<int:pk>',views.TrackDetails.as_view(),name="track.details"),
     path('update/<int:pk>',views.TrackUpdate.as_view(),name="track.Update"),
     path('delete/<int:pk>',views.Trackdelete.as_view(),name="track.delete"),
