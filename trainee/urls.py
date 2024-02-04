04c1e210f20a0502a09dfb83b8f595ed093a1489
jboles <eng.jospheenboles@gmail.com>
2024-02-04 19:33:24 +0200

    Day4 modelform

--- trainee/urls.py
+++ trainee/urls.py
@@ -4,6 +4,7 @@ urlpatterns=[
     path('List',views.traineelist,name="trainee.list"),
     path('add',views.traineeadd,name="trainee.add"),
     path('addForm',views.traineeaddForm,name="trainee.addForm"),
+    path('addFormModel',views.traineeaddFormModel,name="trainee.addFormModel"),
     path('<int:id>',views.traineedetails,name="trainee.details"),
     path('Update/<int:id>',views.traineeupdate,name="trainee.update"),
     path('Delete/<int:id>',views.traineeDelete,name="trainee.Delete"),
