8365397b9505f0678de0fe60c26999d9fda6d8bc
jboles <eng.jospheenboles@gmail.com>
2024-02-05 20:54:58 +0200

    Day4 api -decorator & response

--- trainee/api/views.py
+++ trainee/api/views.py
@@ -1,4 +1,7 @@
 from django import views
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import  Response
+
 def hellapi(request):
     return JsonResponse({"id":1,"name":"ali"})
