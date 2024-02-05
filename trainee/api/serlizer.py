83b473c52a4ed1a09325eadaac51176c46c338e8
jboles <eng.jospheenboles@gmail.com>
2024-02-05 23:15:19 +0200

    Day4 api -retrive objects --->serlizer allow image be null

--- trainee/api/serlizer.py
+++ trainee/api/serlizer.py
@@ -4,6 +4,6 @@ from trainee.models import *
 class Traineeserlizer(serializers.Serializer):
     id=serializers.IntegerField(read_only=True)
     name=serializers.CharField(validators=[UniqueValidator(queryset=Trainee.objects.all())],max_length=50)
-    img=serializers.ImageField()
+    img=serializers.ImageField(allow_empty_file=True)
     createdat=serializers.DateTimeField(read_only=True)
 
