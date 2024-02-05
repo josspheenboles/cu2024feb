ad6b63ccd066320d837f7db172554bea0cf42fc1
jboles <eng.jospheenboles@gmail.com>
2024-02-05 21:06:05 +0200

    Day4 api -accept data

--- trainee/api/urls.py
+++ trainee/api/urls.py
@@ -3,4 +3,5 @@ from .views import *
 urlpatterns=[
     path('Hello/',hell),
     path('HelloAPI/',hellapi),
+    path('acceptdata/',accept_data),
 ]
--- trainee/api/views.py
+++ trainee/api/views.py
@@ -8,3 +8,11 @@ def hell(request):
 @api_view(['GET'])
 def hellapi(request):
     return Response({"id":1,"name":"ali"})
+
+@api_view(['GET',"POST"])
+def accept_data(request):
+    if(request.method=='POST'):
+        return JsonResponse(
+            {"data":request.data,"msg":"request recived"},
+            status=200)
+    return JsonResponse({"data":"get request received"})
