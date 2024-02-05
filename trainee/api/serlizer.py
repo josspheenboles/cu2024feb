25c54b543272e3ed071e3160766cd56b20061b40
jboles <eng.jospheenboles@gmail.com>
2024-02-05 22:37:47 +0200

    Day4 api -retrive objects --->use serlizers

---
+++ trainee/api/serlizer.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+class Traineeserlizer(serializers.Serializer):
+    id=serializers.IntegerField(read_only=True)
+    name=serializers.CharField(max_length=50)
+    image=serializers.ImageField()
+    createdat=serializers.DateTimeField(read_only=True)
